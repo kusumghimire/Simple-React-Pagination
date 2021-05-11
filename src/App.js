5f0bd4383725bfd35d1e1189f8c88c722ac4592c
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-05-11 23:01:08 +0545

    docs: axios, bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React ,{useEffect, useState} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [posts, setPosts] =useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(()=>{
+    const fetchPosts = async() =>{
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    }
+  })
   return (
     <div className="container">
    
